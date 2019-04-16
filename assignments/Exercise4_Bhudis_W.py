70d3279c9ca611c9db121bac105aee8c3d3ebf4c
Bhudis <bwit99@hotmail.com>
2019-04-16 18:13:55 +0700

    Create Exercise4_Bhudis_W.py

---
+++ assignments/Exercise4_Bhudis_W.py
@@ -0,0 +1,9 @@
+ScoreFE = 75.5
+ScoreGB = 81.3
+ScoreICS = 85.0
+ScoreCP = 82.5
+print("---Your Score---")
+print("Foundstion English : ", ScoreFE)
+print("General Business : ", ScoreGB)
+print("Introduction to Computer Systems : ", ScoreICS)
+print("Computer Programming : ", ScoreCP)
